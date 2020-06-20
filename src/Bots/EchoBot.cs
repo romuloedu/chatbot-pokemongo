cc1b7aeb107e46417e8eaf1bb5b486379e381c8c
Romulo Eduardo <romuloedu@gmail.com>
2020-06-19 21:10:24 -0300

    - Criação do método de interceptação das mensagens.

--- src/Bots/EchoBot.cs
+++ src/Bots/EchoBot.cs
@@ -1,9 +1,21 @@
 using Microsoft.Bot.Builder;
+using Microsoft.Bot.Schema;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace ChatBot.PokemonGo.Bots
 {
     public class EchoBot : ActivityHandler
     {
+        protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext,
+        CancellationToken cancellationToken)
+        {
+            string reply = $"Echo: {turnContext.Activity.Text}";
 
+            await turnContext.SendActivityAsync(
+                MessageFactory.Text(reply),
+                cancellationToken
+            );
+        }
     }
 }
